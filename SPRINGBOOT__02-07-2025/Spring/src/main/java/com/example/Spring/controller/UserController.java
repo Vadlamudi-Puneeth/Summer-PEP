df44653317111aa9fbaa575e5b99c4ae7fabe494
Vadlamudi Puneeth <118372146+Vadlamudi-Puneeth@users.noreply.github.com>
2025-07-03 11:13:56 +0530

    Add files via upload

---
+++ SPRINGBOOT__02-07-2025/Spring/src/main/java/com/example/Spring/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.example.Spring.controller;
+
+import com.example.Spring.model.User;
+import com.example.Spring.repository.UserRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity<List<User>> createUser(@Valid @RequestBody List<User> users) {
+        List<User> savedUsers = userRepository.saveAll(users);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUsers);
+    }     
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+        Optional<User> user = userRepository.findById(id);
+        return user.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @Valid @RequestBody User userDetails){
+        return userRepository.findById(id).map(existingUser ->{
+            existingUser.setName(userDetails.getName());
+            existingUser.setEmail(userDetails.getEmail());
+            User updatedUser = userRepository.save(existingUser);
+            return ResponseEntity.ok(updatedUser);
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        if(userRepository.existsById(id)){
+            userRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
